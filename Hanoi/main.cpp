1cbf15a7a239f36d2ab4b70f2dbf3fe572a782bb
Johnson Cheng <clare8151214@gmail.com>
2020-12-17 01:12:19 +0800

    wright some code

--- Hanoi/main.cpp
+++ Hanoi/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
-
+void hanoi(int n,int p1,int p2,int p3)
+{
+    if(n==1)
+        cout<<"plate move from "<<p1<<" to "<<p3<<endl;
+    else
+    {
+        hanoi(n-1,p1,p3,p2);
+        cout<<"plate move from "<<p1<<" to "<<p3<<endl;
+        hanoi(n-1,p2,p1,p3);
+    }
+}
 int main() {
-    cout<<"hhh"<<endl;
+    int n;
+    cin>>n;
+    hanoi(n,1,2,3);
     return 0;
 }

c93ffcbb48c36e5ba7c932e11993315e66f6f652
Johnson Cheng <clare8151214@gmail.com>
2020-12-18 01:26:07 +0800

    稀疏矩陣建立

---
+++ Sparse_Matrix/cmake-build-debug/CMakeFiles/Sparse_Matrix.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sparse_Matrix.dir/main.cpp.obj"
+  "Sparse_Matrix.exe"
+  "Sparse_Matrix.exe.manifest"
+  "Sparse_Matrix.pdb"
+  "libSparse_Matrix.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sparse_Matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Sparse_Matrix/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+const int ROWS = 8;
+const int COLS = 9;
+const int NOTZERO = 8;
+
+
+int main() {
+    int i,j,tmprw,tmpcl,tmpnz;
+    int temp = 1;
+    int sparse[ROWS][COLS] = { };
+    int compress[NOTZERO][3];
+    srand(time(NULL));
+    tmpnz = NOTZERO;
+    for (i=1;i<tmpnz+1;i++)
+    {
+
+    }
+    
+    return 0;
+}

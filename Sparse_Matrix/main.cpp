6bd12cd41d375109aaa8e49e5b190a9465a194f3
Johnson Cheng <clare8151214@gmail.com>
2020-12-23 17:59:42 +0800

    完成稀疏矩陣

--- Sparse_Matrix/main.cpp
+++ Sparse_Matrix/main.cpp
@@ -16,8 +16,41 @@ int main() {
     tmpnz = NOTZERO;
     for (i=1;i<tmpnz+1;i++)
     {
-
+        tmprw = rand() % ROWS;
+        tmpcl = rand() % COLS;
+        if(sparse[tmprw][tmpcl] != 0)
+            tmpnz++;
+        sparse[tmprw][tmpcl] = i;
     }
-    
+    cout<<"sparse matrix's element"<<endl;
+    for(i=0;i<ROWS;i++)
+    {
+        for(j=0;j<COLS;j++)
+            cout<<"["<<sparse[i][j]<<"] ";
+        cout<<endl;
+    }
+    compress[0][0] = ROWS;
+    compress[0][1] = COLS;
+    compress[0][2] = NOTZERO;
+    for(i=0;i<ROWS;i++)
+    {
+        for(j=0;j<COLS;j++)
+        {
+            if(sparse[i][j] != 0)
+            {
+                compress[temp][0] = i;
+                compress[temp][1] = j;
+                compress[temp][2] = sparse[i][j];
+                temp++;
+            }
+        }
+    }
+    cout<<"compress matrix"<<endl;
+    for(i=0;i<NOTZERO+1;i++)
+    {
+        for(j=0;j<3;j++)
+            cout<<"["<<compress[i][j]<<"] ";
+        cout<<endl;
+    }
     return 0;
 }
